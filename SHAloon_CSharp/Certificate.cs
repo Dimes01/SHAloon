f2b29fb40f4af0b6aa6832d24eeaf3f778ef604f
Дмитрий Тяпин <tyapin2002@mail.ru>
2023-11-12 23:39:38 +0400

    fix: добавить реализацию получения объектов класса Certificate из DLL ps: появляется ошибка "Unable to load DLL 'SHAloon.dll'" (0x8007007E)

---
+++ SHAloon_CSharp/Certificate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHAloon_CSharp
+{
+    public class Certificate
+    {
+        public string Subject { get; private set; } = "";
+        public string PublicKey { get; private set; } = "";
+
+    }
+}
